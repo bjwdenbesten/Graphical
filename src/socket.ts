8400226c8385d3768dc1146cf76e1b1398bb680a
bjwdenbesten <bjwdenbesten@gmail.com>
2025-08-18 20:05:35 -0700

    a

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const url = import.meta.env.VITE_CLIENT_URL || "http://localhost:3000";
+const url = import.meta.env.CLIENT_URL || "http://localhost:3000";
 
 export const socket = io(url);
